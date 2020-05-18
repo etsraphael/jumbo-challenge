ca61b8508605b8a8d975bcadf13eceaf7b36f0d7
etsraphael <ets_raphael@outlook.fr>
2020-05-19 02:26:13 +1000

    update with home background clean

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // default
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  // home
+  { path: 'home', component: HomeComponent },
+
+  // movie details
+  { path: 'movies/:id', component: MovieDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
 export class AppRoutingModule { }
