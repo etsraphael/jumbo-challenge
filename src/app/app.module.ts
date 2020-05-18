b1394bfda95df2a75515949c44c3552d2bea6aee
etsraphael <ets_raphael@outlook.fr>
2020-05-19 03:40:16 +1000

    saved for tonight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { CardComponent } from './core/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
