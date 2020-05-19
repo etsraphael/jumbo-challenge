1494b23586a283ac88bbe8b236e1995b025ab997
etsraphael <ets_raphael@outlook.fr>
2020-05-19 16:20:06 +1000

    before add the comeback button

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -38,4 +38,14 @@ export class MovieDetailComponent implements OnInit {
 
   }
 
+
+  timeConvert(n: number) {
+    var num = n;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return rhours + " hour(s) and " + rminutes + " minute(s)";
+  }
+
 }
